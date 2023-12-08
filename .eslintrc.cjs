eb00dc4ee1ac076abead0424a9992a3215176e6c
Vladislav <dreamervt8@gmail.com>
2023-12-08 15:50:08 +0600

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  rules: {
+    indent: ["warn", 2, { SwitchCase: 1 }], // use 2 space
+    semi: ["warn", "always"], // use ;
+    "no-console": "warn", // don't build console.log()
+    "prefer-const": "warn", // use 'const' for constants instead of 'let'
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+};
