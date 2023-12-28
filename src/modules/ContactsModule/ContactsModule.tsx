2017f484f50bfe2aec2d647c640c13e0ac2804dc
Vladislav <dreamervt8@gmail.com>
2023-12-28 21:04:15 +0600

    add feddback form, init contacts info

--- src/modules/ContactsModule/ContactsModule.tsx
+++ src/modules/ContactsModule/ContactsModule.tsx
@@ -1,5 +1,19 @@
+import { Container, Grid } from "@mui/material";
 import React from "react";
+import { FeedbackForm } from "../../components/FeedbackForm";
+import { ContactsInfo } from "./components/ContactsInfo";
 
 export const ContactsModule = () => {
-  return <div>ContactsModule</div>;
+  return (
+    <Container>
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={7}>
+          <FeedbackForm />
+        </Grid>
+        <Grid item xs={12} md={5}>
+          <ContactsInfo />
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
