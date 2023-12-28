b9b403a46b19b3c5917613cde85231d1a3c3a86e
Vladislav <dreamervt8@gmail.com>
2023-12-28 21:08:18 +0600

    add copy func

--- src/modules/ContactsModule/components/ContactsInfo/ContactsInfo.tsx
+++ src/modules/ContactsModule/components/ContactsInfo/ContactsInfo.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { Box, IconButton, Typography } from "@mui/material";
 import { FaCopy } from "react-icons/fa6";
+import toast from "react-hot-toast";
 
 export const ContactsInfo = () => {
+  const handleCopyClipboard = (value: string) => {
+    navigator.clipboard
+      .writeText(value)
+      .then(() => {
+        toast.success("Успешно скопировано!");
+      })
+      .catch((err) => {
+        toast.error(
+          "Не удалось скопировать, возможно Ваш браузер заблокировал это действие",
+        );
+        // eslint-disable-next-line no-console
+        console.error("Ошибка при копировании: ", err);
+      });
+  };
+
   return (
     <Box>
       <Typography component="h3" variant="titleSecondRegular">
@@ -30,7 +46,10 @@ export const ContactsInfo = () => {
             >
               support@roze.kz
             </Typography>
-            <IconButton sx={{ marginLeft: 0.5 }}>
+            <IconButton
+              sx={{ marginLeft: 0.5 }}
+              onClick={() => handleCopyClipboard("support@roze.kz")}
+            >
               <FaCopy size="14" color="hsla(213, 100%, 53%, 1)" />
             </IconButton>
           </Box>
@@ -56,7 +75,10 @@ export const ContactsInfo = () => {
             >
               b2b@roze.kz
             </Typography>
-            <IconButton sx={{ marginLeft: 0.5 }}>
+            <IconButton
+              sx={{ marginLeft: 0.5 }}
+              onClick={() => handleCopyClipboard("b2b@roze.kz")}
+            >
               <FaCopy size="14" color="hsla(213, 100%, 53%, 1)" />
             </IconButton>
           </Box>
@@ -77,12 +99,15 @@ export const ContactsInfo = () => {
           >
             <Typography
               component="a"
-              href="tel:+7 775 281 3783"
+              href="tel:+77752813783"
               color="customColors.colorsBlue"
             >
               +7 775 281 3783
             </Typography>
-            <IconButton sx={{ marginLeft: 0.5 }}>
+            <IconButton
+              sx={{ marginLeft: 0.5 }}
+              onClick={() => handleCopyClipboard("+77752813783")}
+            >
               <FaCopy size="14" color="hsla(213, 100%, 53%, 1)" />
             </IconButton>
           </Box>
