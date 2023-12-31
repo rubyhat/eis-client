b79a8b67f73c0ad74dc594887cb81a3657569080
Vladislav <dreamervt8@gmail.com>
2023-12-31 13:59:06 +0600

    add msg to console

--- src/modules/AppModule/components/Footer/Footer.tsx
+++ src/modules/AppModule/components/Footer/Footer.tsx
@@ -1,11 +1,29 @@
-import { Box, Container, Grid, Typography } from "@mui/material";
+/* eslint-disable no-console */
 import React from "react";
+import { Box, Container, Grid, Typography } from "@mui/material";
+
 import { Logotype } from "../../../../components/Logotype";
 import { MenuList } from "../MenuList";
 import { ContactsInfo } from "../../../ContactsModule/components/ContactsInfo";
 import { version } from "../../../../../package.json";
 
 export const Footer = () => {
+  React.useEffect(() => {
+    const styles = [
+      "color: white",
+      "font-size: 12px",
+      "background-color: hsla(211, 100%, 50%, 1)",
+      "padding: 4px",
+      "border-radius: 4px",
+      "font-weight: bold",
+    ].join(";");
+    console.log(`%cv${version}`, styles, "Platform Version");
+    console.log(
+      `%csupport@roze.kz`,
+      styles,
+      `Если вы столкнулись с проблемой, пожалуйста сообщите нам`,
+    );
+  }, []);
   return (
     <Box
       component="footer"
