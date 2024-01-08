f78fa2ac46f921cf89d648ec426d26d97c85fe20
Vladislav <dreamervt8@gmail.com>
2024-01-08 13:49:49 +0600

    use metatag hook in product details

--- src/modules/EstateDetailsModule/EstateDetailsModule.tsx
+++ src/modules/EstateDetailsModule/EstateDetailsModule.tsx
@@ -5,11 +5,18 @@ import { AgentCard } from "../../components/AgentCard/AgentCard";
 import { DetailsList } from "./components/DetailsList/DetailsList";
 import { ImageViewer } from "./components/ImageViewer/ImageViewer";
 import { DetailsSkeleton } from "./components/DetailsSkeleton";
+import { useDynamicMetaTags } from "../../hooks/useDynamicMetaTags";
 
 export const EstateDetailsModule = () => {
   useTitle("Детали объекта недвижимости");
   // todo: upd skelete when data will be received
   const [isLoading] = React.useState(false);
+  // todo: add dynamic data
+  useDynamicMetaTags({
+    title: "Проспект Нуркена Абдирова, 124/3а",
+    description:
+      "Тихий двор, неугловая, кухня-студия, улучшенная, пластиковые окна, счётчики. Здравствуйте уважаемые покупатели!",
+  });
 
   if (isLoading) {
     return <DetailsSkeleton />;
