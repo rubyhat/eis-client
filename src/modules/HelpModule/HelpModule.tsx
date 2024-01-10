a0fcce989fb92360bfe23e5e79c669d4b640e8da
Vladislav <dreamervt8@gmail.com>
2024-01-10 22:33:38 +0600

    init help module

---
+++ src/modules/HelpModule/HelpModule.tsx
@@ -0,0 +1,12 @@
+import { Container, Grid } from "@mui/material";
+import React from "react";
+
+export const HelpModule = () => {
+  return (
+    <Container>
+      <Grid container spacing={2}>
+        <Grid item xs={12}></Grid>
+      </Grid>
+    </Container>
+  );
+};
--- src/pages/Help/Help.tsx
+++ src/pages/Help/Help.tsx
@@ -1,6 +1,11 @@
 import { Box } from "@mui/material";
 import React from "react";
+import { HelpModule } from "../../modules/HelpModule";
 
 export const Help = () => {
-  return <Box className="section"></Box>;
+  return (
+    <Box className="section">
+      <HelpModule />
+    </Box>
+  );
 };
