7c8c94aca4ef04e89404b958839f2ca196077602
Vladislav <dreamervt8@gmail.com>
2024-03-05 17:07:07 +0600

    add x-service-id header

--- src/axiosConfig/axiosConfig.ts
+++ src/axiosConfig/axiosConfig.ts
@@ -4,9 +4,9 @@ const API_BASE_PATH = import.meta.env.VITE_API_BASE_PATH;
 
 export const axiosBaseWrap = axios.create({
   baseURL: API_BASE_PATH,
-  // withCredentials: true,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
+    "X-Service-Id": "client",
   },
 });
