ad31559dbc724831d4116d98da973ea5fba9ae02
Vladislav <dreamervt8@gmail.com>
2024-03-17 21:34:14 +0500

    upd image viewer modal

--- src/modules/EstateDetailsModule/components/ImageViewerModal/ImageViewerModal.tsx
+++ src/modules/EstateDetailsModule/components/ImageViewerModal/ImageViewerModal.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Box, IconButton, SwipeableDrawer, Typography } from "@mui/material";
+import { IoClose } from "react-icons/io5";
+
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/scss";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 import { useScreenSize } from "../../../../hooks/useScreenSize";
 import { useEstateDetailsStore } from "../../store/useEstateDetailsStore";
 import { CustomButton } from "../../../../components/CustomButton";
-
-import "swiper/scss";
-import { IoClose } from "react-icons/io5";
 
 export const ImageViewerModal = () => {
   const {
@@ -46,14 +49,14 @@ export const ImageViewerModal = () => {
       onOpen={handleOpenViewer}
       PaperProps={{
         sx: {
+          justifyContent: "space-between",
           width: "100%",
-          // maxWidth: "1236px",
-          borderRadius: isMobile ? "8px 8px 0 0" : 4,
+          height: "100%",
+          backgroundColor: "rgba(0,0,0, 1)",
+          borderRadius: 0,
           margin: {
             sx: "none",
-            // sm: "10% auto",
           },
-          // height: "90vh",
         },
       }}
     >
@@ -68,7 +71,7 @@ export const ImageViewerModal = () => {
                 width: "100%",
                 height: 4,
                 borderRadius: 4,
-                backgroundColor: "customColors.labelsQuaternary",
+                backgroundColor: "hsla(29, 100%, 50%, .75)",
               }}
             ></Box>
           </Box>
@@ -86,7 +89,7 @@ export const ImageViewerModal = () => {
             Просмотр фотографий
           </Typography>
           <IconButton onClick={handleCloseViewer}>
-            <IoClose />
+            <IoClose color="hsla(29, 100%, 50%, 1)" />
           </IconButton>
         </Box>
       )}
@@ -101,10 +104,13 @@ export const ImageViewerModal = () => {
           }}
         >
           <Swiper
-            slidesPerView={1.1}
-            spaceBetween={8}
+            modules={isMobile ? [Pagination] : [Navigation, Pagination]}
+            slidesPerView={1}
+            spaceBetween={0}
             initialSlide={initialSlide || 0}
             speed={666}
+            navigation={!isMobile}
+            pagination={{ clickable: true }}
           >
             {images &&
               images.map((image, index) => (
@@ -113,10 +119,9 @@ export const ImageViewerModal = () => {
                     component="img"
                     src={image.imageUrl}
                     sx={{
-                      borderRadius: 3,
                       width: 1, // занимает 100% ширины контейнера
-                      height: "auto", // автоматическая высота для сохранения пропорций
-                      maxHeight: "90vh",
+                      height: { xs: "inherit", sm: "calc(100vh - 100px)" }, // автоматическая высота для сохранения пропорций
+                      // maxHeight: "90vh",
                       maxWidth: "100%", // максимальная ширина ограничена шириной контейнера
                       objectFit: "contain", // сохраняет пропорции изображения
                     }}
@@ -125,19 +130,19 @@ export const ImageViewerModal = () => {
               ))}
           </Swiper>
         </Box>
-        {isMobile && (
-          <Box display="flex" padding="8px 0 16px 0">
-            <CustomButton
-              onClick={handleCloseViewer}
-              fullWidth
-              size="large"
-              sx={{ margin: "0 16px" }}
-            >
-              Закрыть
-            </CustomButton>
-          </Box>
-        )}
       </Box>
+      {isMobile && (
+        <Box display="flex" padding="8px 0 16px 0">
+          <CustomButton
+            onClick={handleCloseViewer}
+            fullWidth
+            size="large"
+            sx={{ margin: "0 16px" }}
+          >
+            Вернуться
+          </CustomButton>
+        </Box>
+      )}
     </SwipeableDrawer>
   );
 };
